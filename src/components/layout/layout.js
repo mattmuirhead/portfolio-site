589bd3fe128ca6b5b7512742f235d23f297f76ea
Matt Muirhead <mat.rob.muirhead@googlemail.com>
2019-04-16 12:55:41 +0100

    Added scroller

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import Header from '../header/header'
+import Header from './header/header'
+import Scroller from './scroller/scroller'
 
 import * as styles from './layout.module.scss'
 
-export default ({ children }) => (
+export default ({ children, hasScroll }) => (
     <div className={styles.siteContainer}>
         <Header />
         {children}
+
+        {hasScroll && <Scroller />}
     </div>
 )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,56 +1,8 @@
 import React from 'react'
-import { Link, graphql } from 'gatsby'
 import Layout from '../components/layout/layout'
 
-interface IndexPageProps {
-	data: {
-	  	site: {
-			siteMetadata: {
-				name: string;
-				slug: string;
-		  		tagline: string;
-			}
-		}
-		allContentfulProjects: {
-			edges: {
-			  	node: {
-					title: string;
-			  	}
-			}
-		}   
-	}
-}
-
-export const indexPageQuery = graphql`
-  	query IndexPageQuery {
-		allContentfulProjects {
-			edges {
-			  	node {
-					title
-					slug
-			  	}
-			}
-		}
-  	}
-`
-  
-export default class IndexPage extends React.Component<IndexPageProps, {}> {
-	public render() {
-
-		const projects = this.props.data.allContentfulProjects.edges.map((edge) => edge.node)
-		  
-		return (
-			<Layout>
-				<ul>
-          			{projects.map((project, index) => {
-						return <li key={index}>
-									<Link to={`/project/${project.slug}`}>
-										{project.title}
-									</Link>
-								</li>
-          			})}
-        		</ul>
-			</Layout>
-	  	)
-	}
-}
+export default () => (
+    <Layout hasScroll>
+        Hello World
+    </Layout>
+)
