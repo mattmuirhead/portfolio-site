b75771ab7d6213295261295c68b207104ac99628
Matt Muirhead <mat.rob.muirhead@googlemail.com>
2019-04-16 17:35:01 +0100

    Fixed SVG errors

--- src/components/layout/header/logo/logo.js
+++ src/components/layout/header/logo/logo.js
@@ -10,10 +10,10 @@ export default () => (
         <div className={styles.logo}>
             <svg viewBox="0 0 257 307">
                 <g>
-                    <path className={styles.path} d=" M 32 273 L 32 165 L 127 237 L 222 165 L 222 59 L 127 134 L 32 59 L 32 165" fill="none" stroke-width="50" stroke="rgb(0,0,0)" stroke-linejoin="miter" stroke-linecap="square" stroke-miterlimit="3"/>
-                    <line className={styles.line} x1="222" y1="274" x2="222" y2="166" stroke-width="50" stroke="rgb(0,0,0)" stroke-linejoin="miter" stroke-linecap="square" stroke-miterlimit="3"/>
-                    <line x1="117" y1="176" x2="-26" y2="69" stroke-width="35" stroke="rgb(255,255,255)" stroke-linejoin="miter" stroke-linecap="square" stroke-miterlimit="3"/>
-                    <line x1="123" y1="293" x2="271" y2="181" stroke-width="35" stroke="rgb(255,255,255)" stroke-linejoin="miter" stroke-linecap="square" stroke-miterlimit="3"/>
+                    <path className={styles.path} d=" M 32 273 L 32 165 L 127 237 L 222 165 L 222 59 L 127 134 L 32 59 L 32 165" fill="none" strokeWidth="50" stroke="rgb(0,0,0)" strokeLinejoin="miter" strokeLinecap="square" strokeMiterlimit="3"/>
+                    <line className={styles.line} x1="222" y1="274" x2="222" y2="166" strokeWidth="50" stroke="rgb(0,0,0)" strokeLinejoin="miter" strokeLinecap="square" strokeMiterlimit="3"/>
+                    <line x1="117" y1="176" x2="-26" y2="69" strokeWidth="35" stroke="rgb(255,255,255)" strokeLinejoin="miter" strokeLinecap="square" strokeMiterlimit="3"/>
+                    <line x1="123" y1="293" x2="271" y2="181" strokeWidth="35" stroke="rgb(255,255,255)" strokeLinejoin="miter" strokeLinecap="square" strokeMiterlimit="3"/>
                 </g>
             </svg>
         </div>
