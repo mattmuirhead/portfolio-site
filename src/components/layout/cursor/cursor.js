23b9cbc49725f7e5471ee33bae410d3975a5f7d5
Matt Muirhead <mat.rob.muirhead@googlemail.com>
2019-04-17 15:15:18 +0100

    Removed paper.js

--- src/components/layout/cursor/cursor.js
+++ src/components/layout/cursor/cursor.js
@@ -4,13 +4,6 @@ import * as styles from './cursor.module.scss'
 
 export default class Cursor extends React.Component {
     componentDidMount () {
-        const script = document.createElement("script");
-    
-        script.src = "https://cdnjs.cloudflare.com/ajax/libs/paper.js/0.12.0/paper-core.min.js";
-        script.async = true;
-    
-        document.body.appendChild(script);
-
         let clientX = -100;
         let clientY = -100;
         const innerCursor = this.refs.cursor;
