ee5c60c60e8dc6bf567ed73511b98f64e43f72a3
Matt Muirhead <mat.rob.muirhead@googlemail.com>
2019-04-26 14:17:54 +0100

    Added dummy content to projects template

--- src/templates/project.js
+++ src/templates/project.js
@@ -1,14 +1,46 @@
 import React from 'react'
-import { Link, graphql } from 'gatsby'
+import { graphql } from 'gatsby'
 import Layout from '../components/layout/layout'
+import Slide from '../components/homepageSlider/SlideLayout/SlideLayout'
+
+import * as styles from './projects.module.scss'
 
 export default ({ data }) => {
-	const project = data.contentfulProjects
+	// const project = data.contentfulProjects
   	return (
 		<Layout>
-			<Link to="/">Home</Link> 
-			<div>{project.title}</div>
-			<div>{project.company}</div>
+			<Slide 
+				title="Some cool quote goes here"
+				subTitle="Small title"
+				linkText="View Project"
+				link="/project/british-horseracing-authority"
+				project />
+
+			<article className={styles.articleWrapper}>
+				<div className={styles.content}>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</div>
+
+				<div className={styles.content}>
+					<img src="https://placeimg.com/1000/600/tech" alt="" />
+				</div>
+
+				<div className={styles.content}>
+					<img src="https://placeimg.com/1000/600/tech" alt="" />
+				</div>
+
+				<div className={styles.content}>
+					<img src="https://placeimg.com/1000/600/tech" alt="" />
+				</div>
+
+				<div className={styles.content}>
+					<img src="https://placeimg.com/1000/600/tech" alt="" />
+				</div>
+
+				<div className={styles.content}>
+					<img src="https://placeimg.com/1000/600/tech" alt="" />
+				</div>
+			</article>
 		</Layout>
   	)
 }
